9da791647fa5d81bdfb92fe2227287f0b0bbec7f
Constantine <cvartan@gmail.com>
2024-10-28 10:18:04 +0300

    Mutex added

--- queue/queue.go
+++ queue/queue.go
@@ -1,25 +1,35 @@
 package queue
 
+import "sync"
+
 type (
 	Queue struct {
 		start, end *node
-		length int
+		length     int
+		mutex      sync.Mutex
 	}
 	node struct {
 		value interface{}
-		next *node
+		next  *node
 	}
 )
 
 // Create a new queue
 func New() *Queue {
-	return &Queue{nil,nil,0}
+	return &Queue{
+		start:  nil,
+		end:    nil,
+		length: 0,
+	}
 }
+
 // Take the next item off the front of the queue
 func (this *Queue) Dequeue() interface{} {
 	if this.length == 0 {
 		return nil
 	}
+	this.mutex.Lock()
+
 	n := this.start
 	if this.length == 1 {
 		this.start = nil
@@ -28,24 +38,31 @@ func (this *Queue) Dequeue() interface{} {
 		this.start = this.start.next
 	}
 	this.length--
+
+	this.mutex.Unlock()
 	return n.value
 }
+
 // Put an item on the end of a queue
 func (this *Queue) Enqueue(value interface{}) {
-	n := &node{value,nil}
+	this.mutex.Lock()
+	n := &node{value, nil}
 	if this.length == 0 {
 		this.start = n
-		this.end = n		
+		this.end = n
 	} else {
 		this.end.next = n
 		this.end = n
 	}
 	this.length++
+	this.mutex.Unlock()
 }
+
 // Return the number of items in the queue
 func (this *Queue) Len() int {
 	return this.length
 }
+
 // Return the first item in the queue without removing it
 func (this *Queue) Peek() interface{} {
 	if this.length == 0 {
